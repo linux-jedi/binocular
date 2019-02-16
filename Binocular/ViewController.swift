7528041a5a4f07dd2ac6cdbde5f855c0755725c3
linux-jedi <calebtho@usc.edu>
2019-02-16 10:43:36 -0800

    Initial Commit

---
+++ Binocular/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Binocular
+//
+//  Created by Caleb Thomas on 2/16/19.
+//  Copyright © 2019 Caleb Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
